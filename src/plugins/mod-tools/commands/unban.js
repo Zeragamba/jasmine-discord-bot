a5ad5969d3facf2e29e7fb512554ef4dad1ef305
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-04-11 13:34:54 -0400

    remove RxJS from !unban

--- src/plugins/mod-tools/commands/unban.js
+++ src/plugins/mod-tools/commands/unban.js
@@ -1,7 +1,4 @@
-const {throwError} = require('rxjs');
-const {flatMap, map, catchError} = require('rxjs/operators');
-
-const {ERRORS} = require('../utility');
+const {ChaosError} = require('chaos-core').errors;
 
 module.exports = {
   name: 'unban',
@@ -16,65 +13,49 @@ module.exports = {
     },
   ],
 
-  run(context, response) {
+  async run(context, response) {
     let userService = this.chaos.getService('core', 'UserService');
 
     let guild = context.guild;
     let userString = context.args.user;
 
-    return userService.findUser(userString).pipe(
-      map((member) => {
-        if (!member) { throw new Error(ERRORS.USER_NOT_FOUND); }
-        return member;
-      }),
-      flatMap((user) => guild.unban(user, `Unbanned by ${context.author.tag}`)),
-      flatMap((user) => response.send({content: `${user.tag} has been unbanned`})),
-      catchError((error) => {
-        if (error.name === 'DiscordAPIError') {
-          switch (error.message) {
-            case "Unknown Ban":
-              response.content = `Looks like that user is not banned.`;
-              break;
-            case "Missing Permissions":
-              response.content =
-                `Whoops, I do not have permission to unban users. Can you check if I have the ` +
-                `"Ban members" permission?`;
-              break;
-            case "Privilege is too low...":
-              response.content =
-                `I'm sorry, but I don't have permission to unban that user. They were banned by someone with ` +
-                `higher permissions then me.`;
-              break;
-            default:
-              response.content = `Err... Discord returned an unexpected error when I tried to unban that user.`;
-              this.chaos.messageOwner(
-                "I got this error when I tried to unban a user:",
-                {
-                  embed: this.chaos.createEmbedForError(error, [
-                    {name: "guild", inline: true, value: context.guild.name},
-                    {name: "channel", inline: true, value: context.channel.name},
-                    {name: "command", inline: true, value: "unban"},
-                    {name: "user to unban", inline: true, value: userString},
-                    {name: "user unbanning", inline: true, value: context.author.tag},
-                  ]),
-                },
-              );
-          }
+    try {
+      const user = await userService.findUser(userString).toPromise();
+      const bans = await guild.fetchBans();
+      if (!bans.get(user.id)) {
+        return response.send({
+          content: `${user.tag} is not banned.`,
+        });
+      }
 
-          return response.send();
-        }
-
-        switch (error.message) {
-          case ERRORS.USER_NOT_FOUND:
-            return response.send({
-              content:
-                `Sorry, but I wasn't able to find that user. I can only find users by User Tag if they are in ` +
-                `another guild I'm on. If you know their User ID I can find them by that.`,
-            });
-          default:
-            return throwError(error);
-        }
-      }),
-    );
+      await guild.unban(user, `Unbanned by ${context.author.tag}`);
+      return response.send({
+        content: `${user.tag} has been unbanned.`,
+      });
+    } catch (error) {
+      if (error instanceof ChaosError) {
+        return response.send({
+          content: error.message,
+        });
+      } else if (error.message === "Unknown Ban") {
+        return response.send({
+          content: `Looks like that user is not banned.`,
+        });
+      } else if (error.message === 'Missing Permissions') {
+        return response.send({
+          content:
+            `Whoops, I do not have permission to ban unban. Can you ` +
+            `check if I have the "Ban members" permission?`,
+        });
+      } else if (error.message === 'Privilege is too low...') {
+        return response.send({
+          content:
+            `I'm sorry, but I don't have permission to unban that user. ` +
+            `They were banned by someone with higher permissions then me.`,
+        });
+      } else {
+        throw error;
+      }
+    }
   },
 };
