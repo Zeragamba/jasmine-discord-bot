59e46ac8e5cdf2c98da6c12eea773d822fc34208
Stephen A. Wilson <Stephen-356@hotmail.com>
2020-04-13 17:55:30 -0400

    remove RxJS from broadcast settings config action

--- src/plugins/ow-mains/config/broadcast-settings.js
+++ src/plugins/ow-mains/config/broadcast-settings.js
@@ -1,35 +1,24 @@
-const {from} = require('rxjs');
-const {flatMap, map, toArray, defaultIfEmpty} = require('rxjs/operators');
 const Discord = require('discord.js');
 
 module.exports = {
   name: 'broadcastSettings',
   description: `Views current broadcast settings`,
 
-  run(context) {
+  async run(context) {
     const broadcastService = this.chaos.getService('owMains', 'BroadcastService');
     let guild = context.guild;
+    const embed = new Discord.RichEmbed();
 
-    return from(broadcastService.broadcastTypes).pipe(
-      flatMap((type) => {
-        return broadcastService.getBroadcastChannel(type, guild).pipe(
-          map((channel) => channel.toString()),
-          map((channel) => [type, channel]),
-          defaultIfEmpty([type, "*none*"]),
-        );
-      }),
-      map(([type, channel]) => {
-        return {name: type, value: channel};
-      }),
-      toArray(),
-      map((fields) => new Discord.RichEmbed({fields})),
-      map((embed) => {
-        return {
-          status: 200,
-          content: "Here are the current broadcast settings:",
-          embed,
-        };
-      }),
-    );
+    for (const type of broadcastService.broadcastTypes) {
+      const channelName = await broadcastService.getBroadcastChannel(type, guild).toPromise()
+        .then((channel) => (channel ? channel.toString() : "*none*"));
+      embed.addField(type, channelName);
+    }
+
+    return {
+      status: 200,
+      content: "Here are the current broadcast settings:",
+      embed,
+    };
   },
 };
